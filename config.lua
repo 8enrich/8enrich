9d420923c7f2ad9460de250b3121a4a514647624
8enrich <henriqueftcunha@gmail.com>
2025-11-18 11:24:49 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -21,3 +21,59 @@ lvim.builtin.gitsigns.opts = {
   attach_to_untracked = true,
   current_line_blame = true,
 }
+vim.wo.relativenumber = true
+lvim.plugins = {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  }
+}
+vim.cmd(
+[[
+function! OpenMarkdownPreview (url)
+  let cmd = "firefox --new-window " . a:url
+  silen call system(cmd)
+endfunction
+]]
+)
+
+vim.g.mkdp_auto_start = 0
+vim.g.mkdp_auto_close = 1
+vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_command_for_global = 0
+vim.g.mkdp_open_to_the_world = 0
+vim.g.mkdp_open_ip = "127.0.0.1"
+vim.g.mkdp_browser = "firefox"
+vim.g.mkdp_echo_preview_url = 0
+vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+vim.g.mkdp_preview_options = {
+    mkit = {},
+    katex = {},
+    uml = {},
+    maid = {},
+    disable_sync_scroll = 0,
+    sync_scroll_type = 'middle',
+    hide_yaml_meta = 1,
+    sequence_diagrams = {},
+    flowchart_diagrams = {},
+    content_editable = false,
+    disable_filename = 0,
+    toc = {}
+}
+vim.g.mkdp_markdown_css = ""
+vim.g.mkdp_highlight_css = ""
+vim.g.mkdp_port = ""
+vim.g.mkdp_page_title = '「${name}」'
+vim.g.mkdp_images_path = '/home/user/.markdown_images'
+vim.g.mkdp_filetypes = { "markdown" }
+vim.g.mkdp_theme = 'dark'
+vim.g.mkdp_combine_preview = 0
+vim.g.mkdp_combine_preview_auto_refresh = 1
+
+vim.keymap.set('t', '<C-n>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { noremap = true, silent = true })
